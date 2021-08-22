ee495f32a86d8b8ab84fc04792982a7c89b50cc7
shrivmudit <muditsrivastava.2421@gmial.com>
2021-08-23 00:45:12 +0530

    MockitoOperationsMethod

--- src/main/java/MockitoArithmeticOperations.java
+++ src/main/java/MockitoArithmeticOperations.java
@@ -1,1 +1,7 @@
 
+public interface MockitoArithmeticOperations {
+    double sum(double input1, double input2);
+    double difference(double input1,double input2);
+    double product(double input1, double input2);
+    double division(double input1, double input2);
+}
